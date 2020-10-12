b7d973bdc9a42b53952770ed4463ea2a7cd83337
niteshseram <niteshseram@gmail.com>
2020-10-12 15:48:16 +0530

    Updates

---
+++ precache-manifest.d994ba8021fe1363fb8375e39b54f5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274e29cad955d68bedc7f1f7dd09b901",
+    "url": "/discord-clone/index.html"
+  },
+  {
+    "revision": "3b83181acf8e5573f3eb",
+    "url": "/discord-clone/static/css/main.46343c23.chunk.css"
+  },
+  {
+    "revision": "bbda6d4f21ceef94e66a",
+    "url": "/discord-clone/static/js/2.2a2a7cf7.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/discord-clone/static/js/2.2a2a7cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b83181acf8e5573f3eb",
+    "url": "/discord-clone/static/js/main.bbb1130a.chunk.js"
+  },
+  {
+    "revision": "7513c738c2e2ffe4d32a",
+    "url": "/discord-clone/static/js/runtime-main.f533448a.js"
+  }
+]);
